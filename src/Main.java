aafd53a7539e581297fc9430263702f92ecac674
aorozcor-coder <aorozcor@unicartagena.edu.co>
2026-05-29 18:01:01 -0500

    Se agrego una condicion a la variable numero

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
             byte numero;
-            String cosa = "";
+            String thing;
         System.out.println("Inicio de Proyecto Adrian felipe orozco romero");
              numero = entrada.nextByte();
-             cosa = entrada.next();
+             thing = entrada.next();
             for (int i = 0; i <= numero; i++ ){
-                System.out.println(i + cosa);
+                System.out.println(i + thing);
 
+            if (numero < 10 && numero > 0){
+                System.out.println("El numero es menor a 10 y mayor a 0");
+            }
             }
     }
 }
